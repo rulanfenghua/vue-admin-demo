1e5b7b5530dcb6695bb45d1a0fd9c6ac7e7db54d
rulanfenghua <rulanfenghua@163.com>
2021-07-24 06:44:05 +0800

    package.json

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '172.16.40.52', // can be overwritten by process.env.HOST
-    // host: 'localhost',
+    // host: '172.16.40.52', // can be overwritten by process.env.HOST
+    host: 'localhost',
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
